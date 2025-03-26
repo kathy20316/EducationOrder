39430248a441a2821d0a7dd6835ef5ed901fdd11
Encho Genchev <esgenchev@gmail.com>
2025-03-26 18:23:27 -0500

    Profile Page attempt 4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
+
+const root = resolve(__dirname, 'src');
+const outDir = resolve(__dirname, 'dist');
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/EducationOrder/',
   plugins: [react()],
+  build:{
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input:{
+        main: resolve(root, 'index.html'),
+        profile: resolve(root, 'profile', 'index.html'),
+      }
+    }
+  }
 })

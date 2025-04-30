7ac3421e589904243e97a04f06e366ed0b04286b
Fabian <ochoa.fabian15@gmail.com>
2025-04-30 13:26:25 -0500

    Changing vite.config.js base from 'EducationOrder' to ''

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   // add base for githubpage
-  base: 'EducationOrder',
+  base: '',
   root: './src',
   build: {
     outDir: "../dist",

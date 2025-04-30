42dbc2d19b7b4d6ffc12f6b02c02d1244c0163fe
kathy20316 <kathy20316@gmail.com>
2025-04-30 01:48:38 -0500

    Finalize merge with resolving conflicts

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-<<<<<<< HEAD
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from debug_toolbar.toolbar import debug_toolbar_urls
+from orders.views import home, view_all_orders
 
 # Schema view for Swagger documentation
 schema_view = get_schema_view(
@@ -39,28 +40,18 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('__debug__/', include('debug_toolbar.urls')),
     # API endpoints
     path('api/users/', include('users.urls', namespace='users')),
     path('api/courses/', include('courses.urls', namespace='courses')),
     path('api/sessions/', include('sessions.urls', namespace='sessions')),
     path('api/payments/', include('payments.urls', namespace='payments')),
+    ath('', view_all_orders, name='view_all_orders')
     
     # Swagger documentation URLs
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-=======
-from django.urls import path, include
-from debug_toolbar.toolbar import debug_toolbar_urls
-from orders.views import home, view_all_orders
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('orders/', include('orders.urls')),
-    path('__debug__/', include('debug_toolbar.urls')),
-    # path('', home, name='home'),
-    path('', view_all_orders, name='view_all_orders')
->>>>>>> azure-backend
 ]
 
 # Serve media files in development
